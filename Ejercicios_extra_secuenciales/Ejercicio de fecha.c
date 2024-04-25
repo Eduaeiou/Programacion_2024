a61ea8f38f5641549ee3ba8ad3e5f186e8962b58
AlexR1407 <alex_damian1407@protonmail.com>
2024-04-25 17:36:55 -0300

    Primer ejercicio modificado

--- Ejercicios_extra_secuenciales/Ejercicio de fecha.c
+++ Ejercicios_extra_secuenciales/Ejercicio de fecha.c
@@ -3,31 +3,54 @@
 #define Escribir printf
 #define Leer scanf
 
-void calendario();
+int calendario();
+int edad();
 
 int main(){
 
-    int fecha=31129999;
+    int fecha=31129999, fechanac, fechaact;
     Escribir("Ingresar fecha de nacimiento en un entero de 8 cifras para la fecha: ");
     Leer("%d",&fecha);
 
-    calendario(fecha);
+    fechanac= calendario(fecha);
 
-    Escribir("Ingresar fecha actual");
+    Escribir("Es igual a %d dias\n\n",fechanac);
+
+    Escribir("Ingresar fecha actual: ");
     Leer("%d",&fecha);
+
+    fechaact= calendario(fecha);
+
+    Escribir("Es igual a %d dias\n\n",fechaact);
+
+    edad=fechaact-fechanac;
+
+    Escribir("")
 
     //Escribir("Ingresar fecha actual en un entero");
 
 return 0;
 }
-void calendario(int fecha)
+int calendario(int fecha)
 {
-    int anios, mes,dia;
+    int anios, mes,dia, edadaux;
     anios=fecha%10000;
     mes=(fecha/10000)%100;
     dia=fecha/pow(10,6);
 
     Escribir("Anios %d\t",anios);
     Escribir("Mes %d\t\t",mes);
-    Escribir("Dia %d\n",dia);
+    Escribir("Dia %d\t\t",dia);
+
+    edadaux= edad(dia, mes, anios);
+
+    return edadaux;
+}
+int edad(int dias, int mes, int anios)
+{
+    int diasaprox;
+
+    diasaprox=dias+mes*30+anios*365;
+
+    return diasaprox;
 }
