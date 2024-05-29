4948a0f9a2b676b6dc3c4b6c410a8547375c10be
AlexR1407 <alex_damian1407@protonmail.com>
2024-05-28 21:18:39 -0300

    Organizando repositorio

---
+++ Ejercicios en clase/Calculadora.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("Ingresar primer valor: ");
+    scanf("%d",&a);
+    printf("Ingresar segundo valor: ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("resultado de la suma: %d\n",c);
+    c=a-b;
+    printf("resultado de la resta: %d\n",c);
+    c=a*b;
+    printf("resultado de la multiplicacion: %d\n",c);
+    c=a/b;
+    printf("resultado de la division: %d\n",c);
+    return 0;
+}
