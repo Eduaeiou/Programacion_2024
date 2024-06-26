fbf103e3b09da22578baf902149c6cb933a848e3
AlexR1407 <alex_damian1407@protonmail.com>
2024-06-26 08:45:49 -0300

    No pude hacer funcionar de alguna manera que 'press any key to continue...'

--- Guias de trabajo/TP1/TpN1Ej050607.c
+++ Guias de trabajo/TP1/TpN1Ej050607.c
@@ -1,31 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define m 1000
+#include <stdbool.h>
+#include <unistd.h>
+
 void Punto5(); void Punto6(); void Punto7();
 
 int main(){
     
     int opc;
+    bool exit = true;
 
+    do{
     printf("Enter the exercise number. According to: \n");
     printf("\t5. Given two numbers a and b, exchange them. \n");
     printf("\t6. Change from km to m \n");
-    printf("0. Exit");
+    printf("\t0. Exit\n");
     printf("Exercise: ");
     scanf("%d",&opc); system("clear");
 
-    switch (opc)
+        switch (opc)
     {
-        case 5: Punto5();
+        case 5: Punto5(); 
+            printf("Press any key to continue... "); sleep(3); getchar(); exit = false; 
             break;
         case 6: Punto6();
             break;
-        case 0:
+        case 0: exit = true;
             break;
         default:
+            printf("Invalid option. Try again");
             break;
     }
-
+    } while(exit!=true);
+    
     return 0;
 }
 //5. Crea un programa que, dados dos valores para a y b, los intercambie.
@@ -36,8 +44,8 @@ void Punto5(){
     printf("Enter a numer for a: "); scanf("%d",&a);
     printf("Enter a numer for b: "); scanf("%d",&b);
     aux=a; a=b; b=aux;
-    printf("Now the numbers are: \n");
-    printf("\ta=%d \t b=%d\n",a,b);
+    printf("Now the numbers are: \ta=%d \t b=%d\n",a,b);
+
 }
 //6. Ingresar por teclado un valor en Km y mostrarlo expresado en metros.
 void Punto6(){
