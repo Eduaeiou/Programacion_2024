2b3e52d1e0d62cb676fafb15f50c2bebdb065f30
AlexR1407 <alex_damian1407@protonmail.com>
2024-06-26 00:58:33 -0300

    Ejercicios modificados

--- Guias de trabajo/TP1/TpN1Ej050607.c
+++ Guias de trabajo/TP1/TpN1Ej050607.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define m 1000
 void Punto5(); void Punto6(); void Punto7();
 
 int main(){
+    
+    int opc;
 
+    printf("Escribir numero del ejercicio. Según: \n");
+    printf("\t5. Dados dos valores para a y b, los intercambia \n");
+    printf("\t6. Un valor en Km y expresado en metros.\n");
+    printf("Punto: ");
+    scanf("%d",&opc); system("clear");
 
+    switch (opc)
+    {
+        case 5: Punto5();
+            break;
+        case 6: Punto6();
+        default:
+            break;
+    }
 
     return 0;
 }
@@ -17,7 +33,7 @@ void Punto5(){
     printf("Ingresar un valor para b: "); scanf("%d",&b);
     aux=a; a=b; b=aux;
     printf("Ahora los valores son: \n");
-    printf("a=%d \t b=%d",a,b);
+    printf("\ta=%d \t b=%d\n",a,b);
 }
 //6. Ingresar por teclado un valor en Km y mostrarlo expresado en metros.
 void Punto6(){
--- gets no funciona.c
+++ gets no funciona.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <string.h>
 
 int main(){
 	
 	char nombre[30];
-	puts("Ingresar un nombre");
-	gets(nombre);
-	
+	puts("Ingresar un nombre: ");
+	fgets(nombre, 30, stdin); //formato para usar fgets
+	puts(nombre);
+
 	return 0;
 	}
