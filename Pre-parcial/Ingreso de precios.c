ae296315cbe318cdb11e0da2d88cb548608a247d
AlexR1407 <alex_damian1407@protonmail.com>
2024-07-02 03:56:46 -0300

    Fue modificado el código para mejor. Aún no funciona por completo, contiene errores

--- Pre-parcial/Ingreso de precios.c
+++ Pre-parcial/Ingreso de precios.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 void main(){
-    float precio=0,aux; int i=0;
+    int i=0; float f_precio=0, a_precio=0;
     bool fin;
-    char ch;
+    char ch;char c_precio[100];
 
     do{printf("Ingresar precio del producto: $");
-    aux=aux+precio;
-    ch = getchar();
-    i++;printf("i %d",i);
-    if(ch=='\n'&&i==3){
-        printf("Deseas seguir operando? S/N");}
-    else{
-            scanf("%f",&precio);
-            ch = getchar();
-            break;}
+    fgets(c_precio,100,stdin);//fgets(nombre_cadena, numero_caracteres, FILE);  la palabra “stdin” cuyo significado es “standard input”.
+    c_precio[strcspn(c_precio, "\n")] = 0; // Elimina el salto de línea al final de la entrada
+
+    //Si la longitud de la entrada (strlen()) es 0, significa que el usuario presionó Enter sin escribir nada
+    if(strlen(c_precio) == 0){
+        i++;printf("i %d",i);
+        break;
+    } else {
+        //sscanf para intentar convertir la entrada a un número entero.
+        if (sscanf(c_precio, "%f", &f_precio) == 1){
+            a_precio=a_precio+f_precio;
+        } else {
+            if(i == 3){
+                printf("Deseas seguir operando? S/N");scanf("%c",&ch);
+                if (ch == 'S' || ch == 's'){
+                    i=0;
+                } //else {i=3;}
+                }
+            }
+        }
 
     }while(i!=3);
-    printf("Total $%.2f",aux);
+    printf("Total $%.2f",f_precio);
 
 
 
